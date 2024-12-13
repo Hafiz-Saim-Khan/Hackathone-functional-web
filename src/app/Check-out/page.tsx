f1c38189ead8b376c646c683b83e53a9fb99d75b
Hafiz-Saim-Khan <nasirmehmood33k@gmail.com>
2024-12-13 19:45:43 +0500

    updatedfilecontent

--- src/app/Check-out/page.tsx
+++ src/app/Check-out/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import { FaAngleDown } from "react-icons/fa";
 import { SiNike } from "react-icons/si";
 import { IoBagOutline } from "react-icons/io5";
@@ -22,65 +22,65 @@ const Checkout = () => {
         <h1 className="font-semibold">How would you like to get your order?</h1>
             <section className=" text-[#7e7e7e] weight-400 text-[16px] ">
              <p>Customs regulation for India require a copy of the recipient's KYC. The address on the KYC needs to match the shipping address. Our courier will contact you via SMS/email to obtain a copy of your KYC. The KYC will be stored securely and used solely for the purpose of clearing customs (including sharing it with customs officials) for all orders and returns. If your KYC does not match your shipping address, please click the link for more information.</p>
-             <button className='underline'>Learn More</button>            
+             <button className="underline">Learn More</button>            
             </section>
-            <div className='h-[82px] my-10 border-2 gap-5 weigth-600 border-black flex items-center pl-5 rounded-lg w-[440px]'>
-                <p><img src="deliever-it.png " className='h-6 w-6' alt="Deliever" /></p>
+            <div className="h-[82px] my-10 border-2 gap-5 weigth-600 border-black flex items-center pl-5 rounded-lg w-[440px]">
+                <p><img src="deliever-it.png " className="h-6 w-6" alt="Deliever" /></p>
                 <p>Deliver it</p>
             </div>
             {/* SECOND SECTION */}
             <section>
-                <h1 className='weight-500 text-[16px] '>Enter your name and address :</h1>
-                <input type="text"placeholder='First Name' className=' mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] ' />
-                <input type="text"placeholder='Last Name' className=' mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] ' />
-                <input type="text"placeholder='Address Line 1' className=' mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] ' />
-                <p className='weight-400 text-[11px] text-[#7e7e7e] ml-8 mt-2 mb-0'>We donot ship to P.O boxes</p>
-                <input type="text"placeholder='Address Line 2' className=' mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] ' />
-                <input type="text"placeholder='Address Line 3' className=' mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] ' />
-                <div className='flex text-black gap-4 mt-5'>
-                   <button className='h-[56px] border-gray-200 border-2 w-[211px]'>Postal Code</button>
-                   <button className='h-[56px] border-gray-200 border-2 w-[211px]'>Locality</button>
+                <h1 className="weight-500 text-[16px] ">Enter your name and address :</h1>
+                <input type="text"placeholder="First Name" className=" mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] " />
+                <input type="text"placeholder="Last Name" className=" mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] " />
+                <input type="text"placeholder="Address Line 1" className=" mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] " />
+                <p className="weight-400 text-[11px] text-[#7e7e7e] ml-8 mt-2 mb-0">We donot ship to P.O boxes</p>
+                <input type="text"placeholder="Address Line 2" className=" mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] " />
+                <input type="text"placeholder="Address Line 3" className=" mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] " />
+                <div className="flex text-black gap-4 mt-5">
+                   <button className="h-[56px] border-gray-200 border-2 w-[211px]">Postal Code</button>
+                   <button className="h-[56px] border-gray-200 border-2 w-[211px]">Locality</button>
                 </div>
-                <div className='flex text-black gap-4  mt-5'>
-                    <div className='gap-14 flex pl-3 items-center h-[56px] border-gray-200 border-2 w-[211px]'>
-                   <button className='text-[#7e7e7e]'> State/Territory </button><p><FaAngleDown /></p></div>
-                   <div  className=' flex pl-3 items-center gap-36 h-[56px] border-gray-200 border-2 w-[211px] weight-400 text-[16px]'>
-                   <button>India</button><p className='h-2 w-2 bg-gray-500 rounded-full'></p></div>
+                <div className="flex text-black gap-4  mt-5">
+                    <div className="gap-14 flex pl-3 items-center h-[56px] border-gray-200 border-2 w-[211px]">
+                   <button className="text-[#7e7e7e]"> State/Territory </button><p><FaAngleDown /></p></div>
+                   <div  className=" flex pl-3 items-center gap-36 h-[56px] border-gray-200 border-2 w-[211px] weight-400 text-[16px]">
+                   <button>India</button><p className="h-2 w-2 bg-gray-500 rounded-full"></p></div>
                 </div>
-                <div className='flex gap-6 my-6'>
+                <div className="flex gap-6 my-6">
                 <input type="checkbox" /><p>Save this address to my profile</p></div>
-                <div className='flex gap-6 '>
+                <div className="flex gap-6 ">
                 <input type="checkbox" /><p>Make this my preffered address</p></div>
 
             </section>
             {/* THIRD SECTION */}
             <section>
-                <h1 className='font-semibold my-7 text-[21px]'>What's your contact information?</h1>
-                <input type="text"placeholder='Email' className=' mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] ' />
-                <p className='weight-400 text-[11px] text-[#7e7e7e] ml-8 mt-2 mb-0'>A confirmation email will be sent after checkout.</p>
-                <input type="text"placeholder='Phone Number' className=' mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] ' />
-                <p className='weight-400 text-[11px] text-[#7e7e7e] ml-8 mt-2 mb-0'>A carrier might contact you to confirm delivery.</p>
+                <h1 className="font-semibold my-7 text-[21px]">What's your contact information?</h1>
+                <input type="text"placeholder='Email' className=" mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] " />
+                <p className="weight-400 text-[11px] text-[#7e7e7e] ml-8 mt-2 mb-0">A confirmation email will be sent after checkout.</p>
+                <input type="text"placeholder="Phone Number" className=" mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] " />
+                <p className="weight-400 text-[11px] text-[#7e7e7e] ml-8 mt-2 mb-0">A carrier might contact you to confirm delivery.</p>
             </section>
                 {/* section 4 */}
             <section>
-            <h1 className='font-semibold my-7 text-[21px]'>What's your PAN?</h1>
-            <p className='text-[16px] weight-400 mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] p-2' >PAN</p>
-            <p className='weight-400 text-[12px] text-[#7e7e7e] ml-8 mt-2 mb-0'>Enter your PAN to enable payment with upi,Net Banking or local <br />card methods </p>
-            <div className='flex gap-5 text-[#7e7e7e] text-sm mb-20 mt-5'><input type="checkbox" /><p>Save PAN details to Nike Profile</p></div>
-            <div className='flex gap-5 text-[#7e7e7e] text-sm'><input type="checkbox" />
-            <div className='w-[500px] '><p>I have read, consent to eShopWorld processing my information </p>
-             <section className='flex'><p>in accordance with the</p><p className='underline mx-2'>Privacy Statement</p>and <p className='underline mx-2'>Cookie Policy. </p></section>
+            <h1 className="font-semibold my-7 text-[21px]">What's your PAN?</h1>
+            <p className="text-[16px] weight-400 mt-4 rounded-md h-[56px] border-2 border-gray-200 pl-5 w-[440px] p-2" >PAN</p>
+            <p className="weight-400 text-[12px] text-[#7e7e7e] ml-8 mt-2 mb-0">Enter your PAN to enable payment with upi,Net Banking or local <br />card methods </p>
+            <div className="flex gap-5 text-[#7e7e7e] text-sm mb-20 mt-5"><input type="checkbox" /><p>Save PAN details to Nike Profile</p></div>
+            <div className="flex gap-5 text-[#7e7e7e] text-sm"><input type="checkbox" />
+            <div className="w-[500px] "><p>I have read, consent to eShopWorld processing my information </p>
+             <section className="flex"><p>in accordance with the</p><p className="underline mx-2">Privacy Statement</p>and <p className="underline mx-2">Cookie Policy. </p></section>
              <p>eShopWorld is a trusted Nike partner.</p>
 
             </div>
 {/*         SECTION 5 */}
             </div>
-            <button className='h-[60px] items-center  text-center pr-14 my-5 rounded-full text-[15px] weight-500 w-[440px] bg-gray-200'>Continue</button>
-            <div className='h-[346px] w-[440px] weight-500'>
-                <p className='text-xl weigth-600'>Delievery</p>
-                <p className='text-[21px] h-12 w-full border-2 mt-7 border-gray-200 items-center text-[#7e7e7e]'>Shipping</p>
-                <p className='text-[21px] h-12 w-full border-2 mt-7 border-gray-200 items-center text-[#7e7e7e]'>Billing</p>
-                <p className='text-[21px] h-12 w-full border-2 mt-7 border-gray-200 items-center text-[#7e7e7e]'>Payment</p>
+            <button className="h-[60px] items-center  text-center pr-14 my-5 rounded-full text-[15px] weight-500 w-[440px] bg-gray-200">Continue</button>
+            <div className="h-[346px] w-[440px] weight-500">
+                <p className="text-xl weigth-600">Delievery</p>
+                <p className="text-[21px] h-12 w-full border-2 mt-7 border-gray-200 items-center text-[#7e7e7e]">Shipping</p>
+                <p className="text-[21px] h-12 w-full border-2 mt-7 border-gray-200 items-center text-[#7e7e7e]">Billing</p>
+                <p className="text-[21px] h-12 w-full border-2 mt-7 border-gray-200 items-center text-[#7e7e7e]">Payment</p>
              </div>
           </section>
         </div>
@@ -88,54 +88,54 @@ const Checkout = () => {
         
          {/* RIGHT SECTION */}
          <section>
-            <h1 className='font-smibold text-[21px] mb-5'>Order Summary</h1>
-            <div className='text-[15px] weight-500 text-[#8d8d8d] flex gap-5 mt-5 mb-2'>
+            <h1 className="font-smibold text-[21px] mb-5">Order Summary</h1>
+            <div className="text-[15px] weight-500 text-[#8d8d8d] flex gap-5 mt-5 mb-2">
               <p>Sub Total</p>
               <p>20 890.00</p>
             </div>
-            <div className='gap-5 text-[15px] weight-500 mb-6 text-[#8d8d8d] flex'>
+            <div className="gap-5 text-[15px] weight-500 mb-6 text-[#8d8d8d] flex">
               <p>Delivery</p>
               <p>Shipping</p>
             </div>
-            <div className='gap-5 text-[15px] weight-500 font-semibold text-black  flex'>
+            <div className="gap-5 text-[15px] weight-500 font-semibold text-black  flex">
               <p>Total</p>
               <p>20 890.00</p>
             </div>
-            <p className='weight-400 my-3 text-[9px]'>(The total reflects the price of your order, including all duties and taxes)</p>
-            <h1 className='font-bold mb-4 text-[15px]'>Arrives Mon, 27 Mar - Wed, 12 Apr</h1>
-            <section className='flex gap-5'>
-                <div className='size-[208px]'><img src="banda.jpeg" alt="" /></div>
+            <p className="weight-400 my-3 text-[9px]">(The total reflects the price of your order, including all duties and taxes)</p>
+            <h1 className="font-bold mb-4 text-[15px]">Arrives Mon, 27 Mar - Wed, 12 Apr</h1>
+            <section className="flex gap-5">
+                <div className="size-[208px]"><img src="banda.jpeg" alt="tea-shirt" /></div>
                 <div>
-                    <p className='weight-500 text-sm'>Nike Dri-Fit</p>
-                    <p className='weight-500 text-sm'>ADV TechKnit</p>
-                    <p className='weight-500 text-sm'>Ultra Men's</p>
-                    <p className='weight-500 text-sm'>Short-Sleeve</p>
-                    <p className='weight-500 text-sm'>Running Top</p>
-                    <p className='weight-400 text-[15px] text-[#8d8d8d]'>Qty 1</p>
-                    <p className='weight-400 text-[15px] text-[#8d8d8d]'>Size L</p>
-                    <p className='weight-400 text-[15px] text-[#8d8d8d]'>3 895.00</p>
+                    <p className="weight-500 text-sm">Nike Dri-Fit</p>
+                    <p className="weight-500 text-sm">ADV TechKnit</p>
+                    <p className="weight-500 text-sm">Ultra Men's</p>
+                    <p className="weight-500 text-sm">Short-Sleeve</p>
+                    <p className="weight-500 text-sm">Running Top</p>
+                    <p className="weight-400 text-[15px] text-[#8d8d8d]">Qty 1</p>
+                    <p className="weight-400 text-[15px] text-[#8d8d8d]">Size L</p>
+                    <p className="weight-400 text-[15px] text-[#8d8d8d]">3 895.00</p>
                 </div>
             </section>
-            <section className='flex  my-9 gap-5'>
-                <div className='size-[208px]'><img src="shoes.jpeg" alt="" /></div>
+            <section className="flex  my-9 gap-5">
+                <div className="size-[208px]"><img src="shoes.jpeg" alt="Shoes" /></div>
                 <div>
-                    <p className='weight-500 text-sm'>Nike Air Max</p>
-                    <p className='weight-500 text-sm'>97 SE Men's</p>
-                    <p className='weight-500 text-sm'>Shoes</p>
-                    <p className='weight-500 text-sm  text-[#8d8d8d]'>Qty 1</p>
-                    <p className='weight-500 text-sm  text-[#8d8d8d]'>Size UK 8</p>
-                    <p className='weight-400 text-[15px] text-[#8d8d8d]'>16 995.00</p>
+                    <p className="weight-500 text-sm">Nike Air Max</p>
+                    <p className="weight-500 text-sm">97 SE Men's</p>
+                    <p className="weight-500 text-sm">Shoes</p>
+                    <p className="weight-500 text-sm  text-[#8d8d8d]">Qty 1</p>
+                    <p className="weight-500 text-sm  text-[#8d8d8d]">Size UK 8</p>
+                    <p className="weight-400 text-[15px] text-[#8d8d8d]">16 995.00</p>
                 </div>
             </section>
             </section>
     </div>
     {/* /FOOTER */}
-    <div className='flex bg-black h-14 mt-[100px] w-[1350px] text-[12px] items-center'>
-        <div className='flex pl-4'>
-            <p className='text-white'><FaLocationPin className='h-4 w-4'/></p><p className='text-white mx-4'>India</p>
-            <p className='ml-3 text-[#8d8d8d]'>2023 Nike,Inc.All Rights Reserved</p>
+    <div className="flex bg-black h-14 mt-[100px] w-[1350px] text-[12px] items-center">
+        <div className="flex pl-4">
+            <p className="text-white"><FaLocationPin className="h-4 w-4"/></p><p className="text-white mx-4">India</p>
+            <p className="ml-3 text-[#8d8d8d]">2023 Nike,Inc.All Rights Reserved</p>
         </div>
-        <div className='text-[#8d8d8d] flex gap-5 mr-28 ml-10'>
+        <div className="text-[#8d8d8d] flex gap-5 mr-28 ml-10">
             <p>Terms of Use </p>
             <p>Terms of Sale</p>
             <p>Privacy Policy</p>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import { BsTwitter } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 import { TfiYoutube } from "react-icons/tfi";
@@ -11,36 +11,38 @@ import { MdLocationOn } from "react-icons/md";
 const Footer = () => {
   return (
     <div>
-      <footer className=' sm:h-[330px] h-[400px]  md:h-[330px] lg:h-[330px] xl:h-[330px] xl:w-[1350px] bg-black mt-[100px] pt-7 md:pl-[10px] lg:pl-[10px] xl:pl-[100px] text-[#FFFFFF] gap-11 sm:text-[11px] text-[8px] md:text-[10px] lg:text-[11px] xl:text-[11px]'>
-        <div className='flex md:gap-3 '>
+    <div>
+      <footer>
+      <footer className=" sm:h-[330px] h-[400px]  md:h-[330px] lg:h-[330px] xl:h-[330px] xl:w-[1350px] bg-black mt-[100px] pt-7 md:pl-[10px] lg:pl-[10px] xl:pl-[100px] text-[#FFFFFF] gap-11 sm:text-[11px] text-[8px] md:text-[10px] lg:text-[11px] xl:text-[11px]">
+        <div className="flex md:gap-3" >
 
-        <section className='h-[166px] w-[245px]'>
-         <p className='mt-5 md:mr-28'>Find A Store</p> 
-         <p className='mt-5'>BECOME A MEMBER</p>
-         <p className='mt-5'>SIGN UP FOR EMAIL</p>
-         <p className='mt-5'>Send Us Feedback</p>
-         <p className='mt-5'>STUDENT DISCOUNTS</p>
+        <section className="h-[166px] w-[245px]">
+         <p className="mt-5 md:mr-28">Find A Store</p> 
+         <p className="mt-5">BECOME A MEMBER</p>
+         <p className="mt-5">SIGN UP FOR EMAIL</p>
+         <p className="mt-5">Send Us Feedback</p>
+         <p className="mt-5">STUDENT DISCOUNTS</p>
         </section>
-        <section className='h-[166px] w-[245px]'>
-          <p className='mt-5 md:mr-28 text-[#7E7E7E]'>Get Help</p>
-          <p className='mt-5 text-[#7E7E7E]'>Order Status</p>
-          <p className='mt-5 text-[#7E7E7E]'>Delivery</p>
-          <p className='mt-5 text-[#7E7E7E]'>Returns</p>
-          <p className='mt-5 text-[#7E7E7E]'>Payment Option</p>
-          <p className='mt-5 text-[#7E7E7E]'>Contact Us On Nike.com Inquiries</p>
-          <p className='mt-5 text-[#7E7E7E]'>Contact Us On All Other Querries</p>
+        <section className="h-[166px] w-[245px]">
+          <p className="mt-5 md:mr-28 text-[#7E7E7E]">Get Help</p>
+          <p className="mt-5 text-[#7E7E7E]">Order Status</p>
+          <p className="mt-5 text-[#7E7E7E]">Delivery</p>
+          <p className="mt-5 text-[#7E7E7E]">Returns</p>
+          <p className="mt-5 text-[#7E7E7E]">Payment Option</p>
+          <p className="mt-5 text-[#7E7E7E]">Contact Us On Nike.com Inquiries</p>
+          <p className="mt-5 text-[#7E7E7E]">Contact Us On All Other Querries</p>
         </section>
-        <section className='h-[166px] w-[245px]'>
-          <p className='mt-5 md:mr-28 text-[#7E7E7E]'>About Nike</p>
-          <p className='mt-5 text-[#7E7E7E]'>News</p>
-          <p className='mt-5 text-[#7E7E7E]'>Carriers</p>
-          <p className='mt-5 text-[#7E7E7E]'>Investors</p>
-          <p className='mt-5 text-[#7E7E7E]'>Sustainability</p>
+        <section className="h-[166px] w-[245px]">
+          <p className="mt-5 md:mr-28 text-[#7E7E7E]">About Nike</p>
+          <p className="mt-5 text-[#7E7E7E]">News</p>
+          <p className="mt-5 text-[#7E7E7E]">Carriers</p>
+          <p className="mt-5 text-[#7E7E7E]">Investors</p>
+          <p className="mt-5 text-[#7E7E7E]">Sustainability</p>
         </section>
-        <section className='flex gap-6 md:ml-4 lg:ml-[200px] xl:ml-[300px] md:mr-5 '>
-        <BsTwitter  className='h-8 w-8 sm:h-[40px] md:h-[40px] lg:h-[40px] xl:h-[40px] sm:w-[40px] md:w-[40px] lg:w-[40px] xl:w-[40px] rounded-full bg-gray-400 p-2 text-gray-900'/>
-        <FaFacebookF  className='h-8 w-8 sm:h-[40px] md:h-[40px] lg:h-[40px] xl:h-[40px] sm:w-[40px] md:w-[40px] lg:w-[40px] xl:w-[40px] rounded-full bg-gray-400 p-2 text-gray-900'/>
-        <TfiYoutube  className='h-8 w-8 sm:h-[40px] md:h-[40px] lg:h-[40px] xl:h-[40px] sm:w-[40px] md:w-[40px] lg:w-[40px] xl:w-[40px] rounded-full bg-gray-400 p-2 text-gray-900'/>
+        <section className="flex gap-6 md:ml-4 lg:ml-[200px] xl:ml-[300px] md:mr-5 ">
+        <BsTwitter  className="h-8 w-8 sm:h-[40px] md:h-[40px] lg:h-[40px] xl:h-[40px] sm:w-[40px] md:w-[40px] lg:w-[40px] xl:w-[40px] rounded-full bg-gray-400 p-2 text-gray-900"/>
+        <FaFacebookF  className="h-8 w-8 sm:h-[40px] md:h-[40px] lg:h-[40px] xl:h-[40px] sm:w-[40px] md:w-[40px] lg:w-[40px] xl:w-[40px] rounded-full bg-gray-400 p-2 text-gray-900"/>
+        <TfiYoutube  className="h-8 w-8 sm:h-[40px] md:h-[40px] lg:h-[40px] xl:h-[40px] sm:w-[40px] md:w-[40px] lg:w-[40px] xl:w-[40px] rounded-full bg-gray-400 p-2 text-gray-900"/>
 
         </section>
         </div>
@@ -48,14 +50,14 @@ const Footer = () => {
 
 
       
-      <div className='flex mt-[100px] gap-20'>
-        <section className='flex'>
-          <p className='text-white mr-3'><MdLocationOn className='h-[15px] w-[18px]' />
-          </p>
-          <p className='text-white sm:mr-7 mr-14 md:mr-7 lg:mr-7 xl:mr-7'>India</p>
-          <p className='text-[#7E7E7E]'>2023,Inc.Nike All Rights Reserved</p>
+      <div className="flex mt-[100px] gap-20">
+        
+        <section className="flex">
+          <p className="text-white mr-3"><MdLocationOn className="h-[15px] w-[18px]"/></p>
+          <p className="text-white sm:mr-7 mr-14 md:mr-7 lg:mr-7 xl:mr-7">India</p>
+          <p className="text-[#7E7E7E]">2023,Inc.Nike All Rights Reserved</p>
         </section>
-        <section className=' sm:hidden md:hidden lg:hidden xl:visible hidden text-[12px]  text-[#7E7E7E] gap-5  ml-[500px]'>
+        <section className=" sm:hidden md:hidden lg:hidden xl:visible hidden text-[12px]  text-[#7E7E7E] gap-5  ml-[500px]">
           <p>Guides</p>
           <p>Terms of Sale</p>
           <p>Terms of Use </p>
@@ -63,8 +65,12 @@ const Footer = () => {
         </section>
       </div>
       </footer>
+      </footer>
+      
 
     </div>
+    </div>
+    
   )
 }
 
